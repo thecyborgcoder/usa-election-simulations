7fca2e51e42dfefc4ddf690145d0e2b949a985c1
thecybordcoder <jram93x@gmail.com>
2024-11-09 15:15:04 +0800

    change distributions

--- distributions.py
+++ distributions.py
@@ -6,17 +6,23 @@ import matplotlib.pyplot as plt
 n = 50000
 samples = []
 
-# Combined distribution
+# Normal distribution
 for _ in range(n):
-    normal_sample = np.random.normal(0.5, 0.2)
+    normal_sample = np.random.normal(0.5, 0.14)
     normal_sample_clipped = max(0, min(1, normal_sample))
-    uniform_sample = np.random.random()
-    combined_sample = (normal_sample_clipped + uniform_sample) / 2
-    samples.append(combined_sample)
+    samples.append(normal_sample_clipped)
 
-# Two random variables
+# Combined distribution
 # for _ in range(n):
-#     sample = random.random() * random.random()
+#     normal_sample = np.random.normal(0.5, 0.15)
+#     normal_sample_clipped = max(0, min(1, normal_sample))
+#     uniform_sample = np.random.random()
+#     combined_sample = (normal_sample_clipped + uniform_sample) / 2
+#     samples.append(combined_sample)
+
+# Three random variables
+# for _ in range(n):
+#     sample = ( random.random() + random.random() + random.random() ) / 3
 #     samples.append(sample)
 
 # Plotting the distribution
